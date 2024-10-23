6e7520b0ae9f11cd9d4cce3b1a7d5effd95df435
Arantzazu Otal <arantzazu.otal.ext@rsi.cajarural.com>
2024-10-23 21:10:21 +0200

    close session

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.example.chatapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var Inflater = menuInflater
+        Inflater.inflate(R.menu.menu_principal, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_salir -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, Inicio::class.java)
+                Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
