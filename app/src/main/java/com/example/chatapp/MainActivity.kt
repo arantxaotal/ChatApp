dd358674234bed2a23900ad14241f291b84edd9d
Arantzazu Otal <arantzazu.otal.ext@rsi.cajarural.com>
2024-10-24 16:47:27 +0200

    main design

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,32 +1,57 @@
 package com.example.chatapp
 
-import android.content.ContentValues.TAG
+import android.R.array
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
-
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
-
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var database: DatabaseReference
     private lateinit var list_view: ListView
+    private lateinit var database: FirebaseDatabase
+    private lateinit var databaseRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fetchUsers()
 
 
 
+
+    }
+
+    private fun fetchUsers() {
+        list_view = findViewById(R.id.usuario_list)
+        databaseRef = FirebaseDatabase.getInstance().getReference("Users/")
+        val array = ArrayList<String>()
+        val eventListener: ValueEventListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (ds in dataSnapshot.children) {
+                    val nombre_usuario = ds.child("nombre_usuario").getValue(String::class.java)
+                    val apellidos_usuario = ds.child("apellidos_usuario").getValue(String::class.java)
+                    array.add(nombre_usuario + " " + apellidos_usuario)
+                }
+                val arrayAdapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, array)
+                list_view.adapter = arrayAdapter
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        }
+        databaseRef.addListenerForSingleValueEvent(eventListener)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
